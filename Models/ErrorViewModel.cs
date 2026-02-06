6b3ad01375cbded023a6702fcbce3eb0ae181e71
Anna Padgett <119985556+padgettanna@users.noreply.github.com>
2026-02-05 18:08:21 -0800

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MortgageLoanCalculator.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
