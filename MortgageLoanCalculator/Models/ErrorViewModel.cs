0c0620f6aa542b99615b9c3b993919b6f9fa622a
Anna Padgett <119985556+padgettanna@users.noreply.github.com>
2026-02-05 18:09:03 -0800

    Add files via upload

---
+++ MortgageLoanCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MortgageLoanCalculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
